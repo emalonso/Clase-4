8d8b687c6893581a2dea685e4b7c081af59ee742
emalonso <52727261+emalonso@users.noreply.github.com>
2019-07-15 21:17:47 -0500

    Add files via upload

---
+++ cliente2.py
@@ -0,0 +1,21 @@
+from temporizador1 import Temporizador
+from time import sleep
+ 
+t = Temporizador()
+
+t.iniciar([0,1,15])
+
+while True:
+    tiempo = t.mostrar_tiempo()
+    print tiempo
+    sleep(0.2)
+    if tiempo == "00 : 00 : 00":
+        break
+    t.retroceder()
+while True:
+    tiempo = t.mostrar_tiempo()
+    print tiempo
+    sleep(0.2)
+    if tiempo == "00 : 01 : 15":
+        break
+    t.avanzar()
